089215b15fd2b09b2216e16f9adec79d3aabdff5
Shota0000 <supercord530@icloud.com>
2022-04-08 20:02:06 +0900

    完成

---
+++ README.md
@@ -0,0 +1,2 @@
+# alertRestock
+楽天市場のiphoneが入荷された場合，lineに通知(TOKENはプライバシー保護のためテキトー)
--- sample.py
+++ sample.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 import requests
-# sleepメソッドのインポート
 from time import sleep
 
 # webdriver managerのインポート
@@ -12,14 +11,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 url = ['https://item.rakuten.co.jp/rakutenmobile-store/iphone-12_64gb_shop-b/',
 'https://item.rakuten.co.jp/rakutenmobile-store/iphone-12_128gb_shop-b/',
-'https://item.rakuten.co.jp/rakutenmobile-store/iphone-13_128gb_bundle_2/']
-
-# url = ['https://item.rakuten.co.jp/aimere/p0260017/?s-id=pc_top_normal_buyagain&rtg=a9c31751bc8a058cbaaaf853b37343be',
-# 'https://item.rakuten.co.jp/aimere/p0260017/?s-id=pc_top_normal_buyagain&rtg=a9c31751bc8a058cbaaaf853b37343be',
-# 'https://item.rakuten.co.jp/aimere/p0260017/?s-id=pc_top_normal_buyagain&rtg=a9c31751bc8a058cbaaaf853b37343be']
+'https://item.rakuten.co.jp/rakutenmobile-store/iphone-13_128gb_bundle_2/',
+'https://item.rakuten.co.jp/rakutenmobile-store/iphone-13-pro-max_128gb_bundle_2/',
+'https://item.rakuten.co.jp/rakutenmobile-store/iphone-13-pro_128gb_bundle_2/'
+]
 
 def line(i):
-    TOKEN = 'Qx1OO4Cm4jjPjkoV5Gw1dMnmW6vFtzuN9aDkPgJ8tlU'
+    TOKEN = 'dummy'
     api_url = 'https://notify-api.line.me/api/notify'
     send_content_01 = "iphoneが 再入荷したよ。申込はこちら→%s"%(url[i])
     #send_content_02 = 'iphone12 128GBが再入荷したよ。申込はこちら→https://item.rakuten.co.jp/rakutenmobile-store/iphone-12_128gb_shop-b/'
@@ -27,9 +25,6 @@ def line(i):
     # 情報を辞書型にする
     TOKEN_dic = {'Authorization': 'Bearer' + ' ' + TOKEN}
     send_dic_01 = {'message': send_content_01}
-    # send_dic_02 = {'message': send_content_02}
-
-    # LINEに通知を送る（200: 成功時、400: リクエストが不正、401: アクセストークンが無効：公式より）
     requests.post(api_url, headers=TOKEN_dic, data=send_dic_01)
       
 
@@ -38,10 +33,9 @@ options = Options()
 options.add_argument('--headless')
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-# BIGLOBE商品ページにアクセス
 
 while True:
-    sleep(10)
+    sleep(60)
     for i in range(len(url)):
         driver.get(url[i])
         res=driver.find_elements_by_class_name("new-cart-button")
@@ -51,9 +45,3 @@ while True:
             line(i)
         else:
             print(i,'入荷待ち')
-        # line(False)
-    # driver1.get('https://item.rakuten.co.jp/rakutenmobile-store/iphone-12_64gb_shop-b/')
-    # driver2.get('https://item.rakuten.co.jp/rakutenmobile-store/iphone-12_128gb_shop-b/')
-    
-driver1.quit()
-driver2.quit()
